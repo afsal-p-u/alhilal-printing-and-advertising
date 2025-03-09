3d01e55a5b1c2e5dbbb26655b1c86be5df3430da
AFSAL P U <afsalpu16@gmail.com>
2025-03-09 23:01:45 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = () => {
                 onClick={toggleMenu}
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent 
                 md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 
-                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent max-lg:hidden"
+                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:hidden"
               >
                 Clients
               </a>
@@ -128,7 +128,7 @@ const Navbar = () => {
                 onClick={toggleMenu}
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent 
                 md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 
-                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent max-lg:hidden"
+                dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:hidden"
               >
                 Location
               </a>
